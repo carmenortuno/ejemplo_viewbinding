695173962b761ab474c7efefee3a307a75c0b3b7
carmenortuno <carmenortuno@elcampico.org>
2024-01-01 22:02:10 +0100

    prueba

---
+++ app/src/main/java/com/example/ejemplo_viewbinding/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.ejemplo_viewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.ejemplo_viewbinding.databinding.ActivityMainBinding
+//https://developer.android.com/topic/libraries/view-binding?hl=es-419#kotlin
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
